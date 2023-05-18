40c851e15c1187401a3176495dfd1a918985569b
Mattos <juliocpvmattos@gmail.com>
2023-05-18 17:44:43 +0100

    inicial Commit

---
+++ src/main/java/com/sequraapi/disbursement/service/Application.java
@@ -0,0 +1,13 @@
+package com.sequraapi.disbursement.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
